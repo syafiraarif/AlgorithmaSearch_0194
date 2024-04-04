b0cefd8b62a64ea06f37b2f7b0585afdc3219353
syairaarif <putrisyafira4444@gmail.com>
2024-04-04 10:52:05 +0700

    memberikan keterangan dalam prosedur input untuk

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -19,7 +19,7 @@ void input()
             cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
     }
 
-
+    //Accept array elements
     cout << "\n--------------------\n";
     cout << " Enter array elements \n";
     cout << "----------------------\n";
