71d3e7e9fa3ca568800c5e27ef5a9a84a6bc725b
syairaarif <putrisyafira4444@gmail.com>
2024-04-04 11:18:55 +0700

    merapikan program

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -49,7 +49,7 @@ void LinearSearch()
             ctr++;
             if (arr[i] == item)
             {
-                cout << "\n" << item << "found at position" << (i + 1) << endl;         //step 6
+                cout << "\n" << item << "found at position: " << (i + 1) << endl;         //step 6
                 break;
             }
             i++;                                                 //step 4
