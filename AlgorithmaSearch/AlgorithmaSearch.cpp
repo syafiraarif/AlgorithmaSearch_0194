7308fd6d1b95307d9e0dc7d42f5a71ee21c8cc3f
syairaarif <putrisyafira4444@gmail.com>
2024-04-04 10:40:48 +0700

    membuat prosedur input

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -7,6 +7,10 @@ int arr[20];        //arr to be searched
 int n;              //number of elements in the array
 int i;              //index of array element
 
+void input()
+{
+
+}
 int main()
 {
     
