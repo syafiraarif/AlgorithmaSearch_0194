098a3de68d03239645654e7db679bbe2e208983c
syairaarif <putrisyafira4444@gmail.com>
2024-04-04 10:48:40 +0700

    membuat cout dialam prosedur input

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -18,6 +18,12 @@ void input()
         else
             cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
     }
+
+
+    cout << "\n--------------------\n";
+    cout << " Enter array elements \n";
+    cout << "----------------------\n";
+
 }
 int main()
 {
